a151c2cf57f06e24db34cdef22d8e0c8ee047ec6
Thibault Twahirwa <ttwahirw@nd.edu>
2020-09-16 21:51:20 -0400

    brute force

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
+using namespace std;
+
 #include <iostream>
+#include <vector>
+
+class AverageOfSubarrayOfSizek {
+public:
+    AverageOfSubarrayOfSizek(int i, vector<int> vector) {
+
+    }
+
+    static vector<double> findAverages(int K, const vector<int> &arr) {
+        vector<double> result(arr.size() - K + 1);
+        for (int i = 0; i <= arr.size() - K; i++) {
+            double sum = 0;
+            for (int j = i; j < i + K; j++) {
+                sum += arr[j];
+            }
+            result[i] = sum / K;
+
+        }
+        return result;
+    };
+
+
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    vector<double> result1 = AverageOfSubarrayOfSizek::findAverages(5, vector < int > {1, 3, 5, 8, 9, 15});
+
+    for (double d : result1) {
+        cout << d << " ";
+    }
+    cout << std::endl;
     return 0;
 }

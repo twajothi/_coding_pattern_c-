b6cd6f63d0be9461b45da6fe1586000f8a95a763
Thibault Twahirwa <ttwahirw@nd.edu>
2020-09-16 21:06:21 -0400

    C++coding

---
+++ cmake-build-debug-coverage/CMakeFiles/coding_pattern.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/coding_pattern.dir/main.cpp.o"
+  "coding_pattern"
+  "coding_pattern.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/coding_pattern.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
